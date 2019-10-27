88d1953025b5301e791c6e115f1c35fcf5e97ed7
lingxiaopua <1083514716@qq.com>
2019-10-27 23:27:11 +0800

    初始

---
+++ src/menu.js
@@ -0,0 +1,14 @@
+var menus = [
+	{
+	  action: "home",
+	  title: "首页",
+	  path:"/index",
+	  items: [{ title: "统计", path: "/dashboard" }]
+	},
+	{
+	  action: "apps",
+	  title: "线路管理",
+	  path:"/item"
+	},
+];
+export default menus;
