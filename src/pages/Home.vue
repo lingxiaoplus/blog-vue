f25afd42a93cf9c6b428586524915f1be53bda88
lingxiao12345 <651121818@qq.com>
2019-10-28 23:02:27 +0800

    基本框架搭建

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,29 +2,73 @@
 	<v-app id="inspire">
 		<link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
 		<v-navigation-drawer v-model="drawer" app>
-			<v-list dense>
-				<v-list-item @click="">
-					<v-list-item-action>
-						<v-icon>mdi-home</v-icon>
-					</v-list-item-action>
-					<v-list-item-content>
-						<v-list-item-title>Home</v-list-item-title>
-					</v-list-item-content>
-				</v-list-item>
-				<v-list-item @click="">
-					<v-list-item-action>
-						<v-icon>mdi-contact-mail</v-icon>
-					</v-list-item-action>
-					<v-list-item-content>
-						<v-list-item-title>Contact</v-list-item-title>
-					</v-list-item-content>
-				</v-list-item>
+
+
+        <v-list dense>
+          <v-list-item avatar>
+            <v-avatar size="40">
+              <img src="https://cdn.vuetifyjs.com/images/john.jpg" alt="John">
+            </v-avatar>
+            <v-list-item-content>
+              <v-list-item-title style="margin-left: 20px;">admin</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      <v-divider/>
+
+			<v-list dense >
+        <v-list-group v-for="item in items" :key="item.title" no-action >
+           <!--一级菜单 -->
+          <v-list-item slot="activator">
+          	<v-list-item-action>
+          		<v-icon>mdi-{{item.action}}</v-icon>
+          	</v-list-item-action>
+          	<v-list-item-content>
+          		<v-list-item-title>{{item.title}}</v-list-item-title>
+          	</v-list-item-content>
+          </v-list-item>
+          <!-- 二级菜单 -->
+          <v-list-item v-for="subItem in item.items" :key="subItem.title" :to="item.path + subItem.path">
+            <v-list-item-content>
+              <v-list-item-title>{{ subItem.title }}</v-list-item-title>
+            </v-list-item-content>
+            <v-list-item-action>
+              <v-icon>{{ subItem.action }}</v-icon>
+            </v-list-item-action>
+          </v-list-item>
+
+        </v-list-group>
+
 			</v-list>
 		</v-navigation-drawer>
 
 		<v-app-bar app color="indigo" dark>
 			<v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-			<v-toolbar-title>Application</v-toolbar-title>
+			<v-toolbar-title>后台管理系统</v-toolbar-title>
+      <v-spacer/>
+
+      <!-- 调色板 -->
+      <v-btn icon color="cyan">
+        <v-icon>mdi-voicemail</v-icon>
+      </v-btn>
+
+
+      <v-menu offset-y>
+          <template v-slot:activator="{ on }">
+            <v-btn icon color="cyan" dark v-on="on">
+              <v-icon>mdi-send</v-icon>
+            </v-btn>
+          </template>
+          <v-list>
+            <v-list-item
+              v-for="(item, index) in items"
+              :key="index"
+            >
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+      </v-menu>
+
 		</v-app-bar>
 
 		<v-content>
@@ -59,6 +103,7 @@
 </template>
 
 <script>
+  import menus from '../menu.js';
 	export default {
 	    props: {
 	      source: String,
@@ -66,6 +111,11 @@
 	    data: () => ({
 	      drawer: null,
 	    }),
+      computed:{
+        items(){
+          return menus;
+        }
+      }
 	  }
 </script>
 
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -3,10 +3,24 @@ import Vue from 'vue';
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
+import colors from 'vuetify/lib/util/colors'
 Vue.use(Vuetify)
 
 export default new Vuetify({
   icons: {
     iconfont: 'mdi',
+  },
+  theme: {
+    themes: {
+      light: {
+              primary: colors.purple,
+              secondary: colors.grey.darken1,
+              accent: colors.shades.black,
+              error: colors.red.accent3,
+      },
+      dark: {
+        primary: colors.blue.lighten3,
+      },
+    },
   },
 });
