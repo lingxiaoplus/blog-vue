f03bd934d2309d5451e4b8bca0eaaf827dd5d497
lingxiaopua <1083514716@qq.com>
2019-10-30 17:57:34 +0800

    对线路进行增删查

--- src/pages/items/NormalLine.vue
+++ src/pages/items/NormalLine.vue
@@ -1,19 +1,25 @@
 <template>
   <div>
 
+    <v-snackbar v-model="snackbar" color="primary" :timeout="2000" :top="true">
+      {{ snackbarText }}
+      <!-- <v-btn dark text @click="snackbar = false">确认</v-btn> -->
+    </v-snackbar>
+
     <v-data-table :headers="headers" :items="desserts" :page.sync="page" :items-per-page="itemsPerPage"
       hide-default-footer class="elevation-1" @page-count="pageCount = $event">
-
-
       <template v-slot:top>
         <v-toolbar flat color="white">
           <v-toolbar-title>普通线路</v-toolbar-title>
           <v-divider class="mx-4" inset vertical></v-divider>
           <v-spacer></v-spacer>
+          <!-- loading条 -->
+          <v-progress-linear :active="loading" :indeterminate="loading" absolute bottom color="yellow darken-2">
+          </v-progress-linear>
 
           <v-dialog v-model="dialog" max-width="500px">
             <template v-slot:activator="{ on }">
-              <v-btn color="primary" dark class="mb-2" v-on="on">添加</v-btn>
+              <v-btn color="primary" dark class="mb-2" v-on="on" @click="editedIndex = -1">添加</v-btn>
             </template>
             <v-card>
               <v-card-title>
@@ -33,7 +39,19 @@
                       <v-text-field v-model="editedItem.cityName" label="城市"></v-text-field>
                     </v-col>
                     <v-col cols="12" sm="6" md="4">
-                      <v-text-field v-model="editedItem.departureTime" label="出发时间"></v-text-field>
+                      <v-menu ref="dateMenu" v-model="dateMenu" :close-on-content-click="false" :return-value.sync="editedItem.departureTime"
+                        transition="scale-transition" offset-y min-width="290px">
+                        <template v-slot:activator="{ on }">
+                          <v-text-field v-model="editedItem.departureTime" label="出发时间" prepend-icon="mdi-calendar-range"
+                            readonly v-on="on"></v-text-field>
+                        </template>
+                        <v-date-picker v-model="editedItem.departureTime" no-title scrollable>
+                          <v-spacer></v-spacer>
+                          <v-btn text color="primary" @click="dateMenu = false">取消</v-btn>
+                          <v-btn text color="primary" @click="onSelectDate(editedItem.departureTime)">确认</v-btn>
+                        </v-date-picker>
+                      </v-menu>
+                      <!-- <v-text-field v-model="editedItem.departureTime" label="出发时间"></v-text-field> -->
                     </v-col>
                     <v-col cols="12" sm="6" md="4">
                       <v-text-field v-model="editedItem.productPrice" label="价格(元)"></v-text-field>
@@ -48,7 +66,7 @@
               <v-card-actions>
                 <v-spacer></v-spacer>
                 <v-btn color="blue darken-1" text @click="dialog = false">取消</v-btn>
-                <v-btn color="blue darken-1" text @click="dialog = false">保存</v-btn>
+                <v-btn color="blue darken-1" text @click="saveProduct">保存</v-btn>
               </v-card-actions>
             </v-card>
           </v-dialog>
@@ -56,8 +74,8 @@
         </v-toolbar>
       </template>
       <template v-slot:item.action="{ item }">
-        <v-icon small class="mr-2" @click="editItem(item)">
-          mdi-edit
+        <v-icon small class="mr-2" @click="editItem(item)" >
+          mdi-pencil
         </v-icon>
         <v-icon small @click="deleteItem(item)">
           mdi-delete
@@ -86,7 +104,7 @@
           <v-card-actions>
             <v-spacer></v-spacer>
             <v-btn color="green darken-1" text @click="deleteDialog = false">取消</v-btn>
-            <v-btn color="green darken-1" text @click="deleteDialog = false">确认</v-btn>
+            <v-btn color="green darken-1" text @click="deleteProduct">确认</v-btn>
           </v-card-actions>
         </v-card>
       </v-dialog>
@@ -118,7 +136,7 @@
           },
           {
             text: '时间',
-            value: 'departureTime'
+            value: 'dateFormat'
           },
           {
             text: '价格 (元)',
@@ -135,56 +153,126 @@
           },
         ],
         desserts: [{
-            id: '1',
-            productNum: '159',
-            productName: 'Frozen Yogurt',
-            cityName: '成都',
-            departureTime: '2018-12-12',
-            productPrice: 1124,
-            productDesc: '没什么好说的',
-            productStatus: 1,
-          },
-        ],
+          id: '1',
+          productNum: '159',
+          productName: 'Frozen Yogurt',
+          cityName: '成都',
+          dateFormat: '2018-12-12',
+          productPrice: 1124,
+          productDesc: '没什么好说的',
+          productStatus: 1,
+        }, ],
         editedItem: {
           productNum: '',
           productName: 0,
           cityName: 0,
-          departureTime: '',
-          productPrice: 0,
-          productDesc: 0,
+          departureTime: '2018-12-12',
+          productPrice: 1000,
+          productDesc: '',
         },
         dialog: false,
         editedIndex: -1,
         deleteDialog: false,
+        selectDate: new Date().toISOString().substr(0, 10),
+        dateMenu: false,
+        loading: false,
+        snackbar: false,
+        snackbarText: '',
+        productId: '',  //当前线路的id
+        on: false,
+        //dateModal: false,
       }
     },
     computed: {
       formTitle() {
-        return this.editedIndex === -1 ? 'New Item' : 'Edit Item'
+        return this.editedIndex === -1 ? '添加线路' : '编辑线路'
       },
     },
     watch: {
-      dialog(val) {
+      /* dialog(val) {
         val || this.close()
-      },
+      }, */
     },
     methods: {
       itemClick(e) {
         console.log("点击", e);
       },
       editItem(e) {
+        this.editedIndex = 1;
+        this.editedItem = e;
         console.log("点击", e);
       },
       deleteItem(e) {
         this.deleteDialog = true;
         console.log("点击", e);
+        this.productId = e.id;
       },
+      onSelectDate(date) {
+        this.dateMenu = false;
+        this.editedItem.departureTime = date;
+        console.log("选择时间", date);
+      },
+      async getProducts() {
+        try{
+          this.loading = true;
+          let response = await this.$http.get("/product?pageNum=" + 1 + "&pageSize=" + 5);
+          console.log("product : ", response.data);
+          this.desserts = response.data.data;
+          let page = parseInt(response.data.total / this.itemsPerPage) + 1;
+          console.log("page:", page)
+          this.pageCount = page;
+        }catch(e){
+         console.log("获取线路失败",e);
+         this.snackbar = true;
+         this.snackbarText = "获取线路失败";
+        }finally{
+          this.loading = false;
+        }
 
-      async getProducts(){
-        console.log("$http  ",this.$http)
-        let response = await this.$http.get("/product");
-        console.log("product : ",response);
       },
+      async saveProduct() {
+        this.dialog = false;
+        this.loading = true;
+        console.log("产品", this.editedItem);
+        try{
+          let departureTime = this.getUnixTime(this.editedItem.departureTime) * 1000;
+          let editData = Object.assign({}, this.editedItem);
+          editData.departureTime = departureTime;
+          let response = await this.$http.post("/product",editData);
+          this.snackbar = true;
+          this.snackbarText = "添加线路成功";
+          console.log("添加线路",response.data);
+          this.getProducts();
+        }catch(e){
+          console.log("添加线路失败",e);
+          this.snackbar = true;
+          this.snackbarText = "添加线路失败";
+        }finally{
+          this.loading = false;
+        }
+      },
+      async deleteProduct(){
+        this.deleteDialog = false;
+        this.loading = true;
+        try{
+          let response = await this.$http.delete("/product/"+this.productId);
+          console.log("删除线路",response.data);
+          this.getProducts();
+        }catch(e){
+          console.log("删除线路失败",e);
+          this.snackbar = true;
+          this.snackbarText = "删除线路失败";
+        }finally{
+          this.loading = false;
+        }
+
+      },
+      getUnixTime(dateStr){
+          var newstr = dateStr.replace(/-/g,'/');
+          var date =  new Date(newstr);
+          var time_str = date.getTime().toString();
+          return time_str.substr(0, 10);
+      }
     },
     created() {
       this.getProducts();
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -5,10 +5,14 @@ import 'vuetify/dist/vuetify.min.css'
 import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
 import colors from 'vuetify/lib/util/colors'
 Vue.use(Vuetify)
-
+//自定义图标
+const MY_ICONS = {
+  logo: "../assets/logo.svg"
+}
 export default new Vuetify({
   icons: {
     iconfont: 'mdi',
+    values: MY_ICONS,
   },
   theme: {
     themes: {
